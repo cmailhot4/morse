de19cacf9dd5cf8995e0f78efaa4322b9a16050f
lowa1234 <bourassa.jean-philippe@carrefour.cegepvicto.ca>
2019-03-28 15:47:31 -0400

    lettre a b c d e f g h i

--- e.py
+++ e.py
@@ -1,9 +1,10 @@
-from gpiozero import LED
-from time import sleep
+def run():
+	from gpiozero import LED
+	from time import sleep
 
-led = LED(17)
+	led = LED(17)
 
-led.on()
-sleep(1)
-led.off()
-sleep(3)
+	led.on()
+	sleep(1)
+	led.off()
+	sleep(3)
